b95be10fa3a8cfd7921bba4bfe632ff7b286c3ad
noah <shnoah@gmail.com>
2015-07-31 01:57:50 +0900

    Create LetterCapitalize.rb
    http://stackoverflow.com/questions/13520162/ruby-capitalize-every-word-first-letter
    
    분석필요

---
+++ LetterCapitalize.rb
@@ -0,0 +1,35 @@
+def LetterCapitalize(str)
+
+  
+  
+  str=str.split.map(&:capitalize).join(' ')
+    
+ 
+  
+  # code goes here
+  return str 
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+LetterCapitalize(STDIN.gets)  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
