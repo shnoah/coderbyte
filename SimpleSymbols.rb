624279c398bde92fe64b31d4fb5b7b98656f6d58
noah <shnoah@gmail.com>
2015-07-31 01:46:49 +0900

    Create SimpleSymbols.rb
    답답해서 못하겠따.........ㄴㅇ리ㅏㅜㄴ이라ㅟㅏ 루비깔아야ㅣㅈ

---
+++ SimpleSymbols.rb
@@ -0,0 +1,56 @@
+def SimpleSymbols(str)
+
+  
+  i=0
+  cnt=0
+  cnt2=0
+  while i<=str.length
+    if str[i]=="+"
+        cnt+=1
+    end
+	i++
+	end
+  
+  str=str.gsub(/[abcdefghijklmnopqrstuvwxyz]/, '+')     
+  i=0
+  
+  while i<str.length
+    if str[i]=="="
+      cnt2+=1
+    end
+    i++
+    end
+  
+  if cnt==cnt2 
+    str="true"
+  else
+    str="false"
+  end
+  
+  # code goes here
+  return str 
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+SimpleSymbols(STDIN.gets)  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
